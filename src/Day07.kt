ba08f4067adaf49bd16ed9b071cf14337c8dd77f
flacchom <david.b.matejka@gmail.com>
2024-01-18 21:01:20 +0100

    adjusted solution to part 1 and added solution to part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,81 +1,93 @@
 fun main() {
+	data class HandWithType(val hand: String, var type: String, val bid: Int)
 
-    data class HandWithType(val replaced: String, val original: String, val type: String, val bid: Int)
+	fun part1(input: List<String>, joker: Boolean): Long {
+		val cardValue = mapOf(
+			'1' to 14,
+			'2' to 13,
+			'3' to 12,
+			'4' to 11,
+			'5' to 10,
+			'6' to 9,
+			'7' to 8,
+			'8' to 7,
+			'9' to 6,
+			'T' to 5,
+			'Q' to 3,
+			'K' to 2,
+			'A' to 1,
+			if (joker) {
+				'J' to 15
+			} else {
+				'J' to 4
+			}
+		)
+		val sortedHands = mutableListOf<HandWithType>()
+		var jokerCount = 0
+		for (line in input) {
+			val (hand, bid) = line.split(" ")
+			val chars = hand.toSortedSet()
+			if (joker) {
+				jokerCount = hand.count { it == 'J' }
+				chars.remove('J')
+			}
+			val type = when (chars.size) {
+				1 -> "0 Five of a kind"
+				4 -> "5 One Pair"
+				5 -> "6 High Card"
+				2 -> {
+					var max = 0
+					for (char in chars) {
+						var counter = 0
+						hand.map { if (it == char) counter++ }
+						if (counter > max) max = counter
+					}
+					if ((max + jokerCount) == 4) "1 Four of a kind"
+					else "2 Full House"
+				}
 
-    fun part1(input: List<String>): Long {
-        val sortedHands = emptyList<HandWithType>().toMutableList()
+				3 -> { //"4 Two Pair or Three of a kind"
+					var max = 0
+					for (char in chars) {
+						var counter = 0
+						hand.map { if (it == char) counter++ }
+						if (counter > max) max = counter
+					}
+					if (max == 3) "3 Three of a kind"
+					else {
+						if (jokerCount > 0) "3 Three of a kind"
+						else "4 Two Pair"
+					}
+				}
 
-        for (line in input) {
-            val (originalHand, bid) = line.split(" ")
-            val (hand, _) = line
-                .replace("T", "E")
-                .replace("J", "D")
-                .replace("Q", "C")
-                .replace("K", "B")
-                .replace("A", "A")
-                //these need to be 1,2,3 -> z,x,y...
-                .replace("1", "Z")
-                .replace("2", "Y")
-                .replace("3", "X")
-                .replace("4", "W")
-                .replace("5", "V")
-                .replace("6", "U")
-                .replace("7", "T")
-                .replace("8", "S")
-                .replace("9", "R")
-                .split(" ")
-           // println(hand)
-            val chars = hand.toSortedSet()
-            chars.println()
-            val type = when (chars.size) {
-                1 -> "0 Five"
-                4 -> "5 One Pair"
-                5 -> "6 High Card"
-                2 -> {
-                    var max = 0
-                    for (char in chars) {
-                        var counter = 0
-                        hand.map { if (it == char) counter++ }
-                        if (counter > max) max = counter
-                    }
-                    if (max == 4) "1 Four"
-                    else "2 Full House"
-                }
-                3 -> { //"4 Two Pair or Three of a kind"
-                    var max = 0
-                    for (char in chars) {
-                        var counter = 0
-                        hand.map { if (it == char) counter++ }
-                        if (counter > max) max = counter
-                    }
-                    if (max == 3) "3 Three"
-                    else "4 Two Pair"
-                }
-                else -> "this shouldn't happen"
-            }
+				else -> "0 Only Jokers"
+			}
 
-            val new = (HandWithType(hand, originalHand, type, bid.toInt()))
-            sortedHands.add(new)
-        }
-        sortedHands.sortWith(compareBy<HandWithType> { it.type[0] }.thenBy { it.replaced[0] }.thenBy { it.replaced[1] }.thenBy { it.replaced[2] }.thenBy { it.replaced[3] }.thenBy { it.replaced[4] })
+			val handWithType = (HandWithType(hand, type, bid.toInt()))
+			sortedHands.add(handWithType)
+		}
+		sortedHands.sortWith(compareBy<HandWithType> { it.type[0] }
+			.thenBy { cardValue[it.hand[0]] }
+			.thenBy { cardValue[it.hand[1]] }
+			.thenBy { cardValue[it.hand[2]] }
+			.thenBy { cardValue[it.hand[3]] }
+			.thenBy { cardValue[it.hand[4]] })
 
-        var sum: Long = 0
-        var i = sortedHands.size
-        for (hand in sortedHands) {
-            println("$hand rank: $i")
-            sum += hand.bid * i
-            i--
-        }
+		var sum: Long = 0
+		var i = sortedHands.size
+		for (hand in sortedHands) {
+			sum += hand.bid * i
+			i--
+		}
 
-        return sum
-    }
+		return sum
+	}
 
-    fun part2(input: List<String>): Long {
+	fun part2(input: List<String>): Long {
+		return part1(input, true)
+	}
 
-        return -1
-    }
-
-    val input = readInput("Day07")
-    part1(input).println()
-    part2(input).println()
+	val input = readInput("Day07")
+	part1(input, false).println()
+	part2(input).println()
 }
