ceb5d6c607ab813cd69eb8a604d402c393b413da
flacchom <david.b.matejka@gmail.com>
2024-01-18 05:32:47 +0100

    solutions to part 1 and 2 of day 12

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,75 +1,67 @@
 fun main() {
 
+	val cache = mutableMapOf<Pair<String, List<Int>>, Long>()
 
-	fun possibilities(s: String?, rules: List<String>): Int {
-
-		if (s.isNullOrBlank() && rules.isEmpty()) {
-			return 1
+	fun possibilities(s: String?, rules: List<Int>): Long {
+		if (s.isNullOrEmpty()) {
+			return if (rules.isEmpty()) 1
+			else 0
 		}
-		if (s.isNullOrBlank()) {
-			return 0
-		}
-		if (rules.isEmpty() && s.contains("#")) {
-			return 0
-		}
-
-
-		if (s.contains('?')) {
-			return possibilities(s.replaceFirst('?', '.'), rules) + possibilities(s.replaceFirst('?', '#'), rules)
-		}
-
-		if (s.first() == '.') {
-			return possibilities(s.drop(1), rules)
-		}
-		if (s.first() == '#') {
-			var i = 0
-			while (i in s.indices && s[i] == '#') {
-				i++
-			}
-			if (i == rules[0].toInt()) {
-				return possibilities(s.drop(i), rules.drop(1))
+		if (rules.isEmpty()) {
+			return if (s.contains("#")) {
+				0
+			} else {
+				1
 			}
 		}
-		return 0
-	}
 
-	fun part1(input: List<String>): Int {
-		var sum = 0
-        for (line in input) {
-            val (s, rules) = line.split(" ")
-			sum += possibilities(s, rules.split(","))
-        }
+		if (Pair(s, rules) in cache.keys) {
+			return cache[Pair(s, rules)]!!
+		}
+
+		// adapted from HyperNeutrino's youtube-video which is more efficient than mine
+		var sum = 0L
+		if (s[0] in "?.") {
+			sum += possibilities(s.drop(1), rules)
+		}
+		if (s[0] in "#?") {
+			if (rules[0] <= s.length && "." !in s.substring(0..<rules[0]) && (rules[0] == s.length || s[rules[0]] != '#')) {
+				sum += possibilities(s.drop(rules[0] + 1), rules.drop(1))
+			}
+		}
+
+		cache[Pair(s, rules)] = sum
 		return sum
 	}
 
-
-	fun part2(input: List<String>): Int {
-		var sum = 0
+	fun part1(input: List<String>): Long {
+		var sum = 0L
 
 		for (line in input) {
 			val (s, rules) = line.split(" ")
-			var firstAext = ""
-			var firstBext = ""
-			for (i in 0..4) {
-				firstAext += "$s?"
-				firstBext += "$rules,"
-			}
-			firstAext = firstAext.dropLast(1)
-			firstBext = firstBext.dropLast(1)
-			println("$firstAext $firstBext")
+			sum += possibilities(s, rules.split(",").map { it.toInt() })
 		}
+		return sum
+	}
+
+	fun part2(input: List<String>): Long {
+		var sum = 0L
 
 		for (line in input) {
 			val (s, rules) = line.split(" ")
-			val tmp = possibilities(s, rules.split(","))
-			println(tmp)
+			val sb1 = StringBuilder(s)
+			val sb2 = StringBuilder(rules)
+			for (i in 0..3) {
+				sb1.append("?$s")
+				sb2.append(",$rules")
+			}
+			val tmp = possibilities(sb1.toString(), sb2.split(",").map { it.toInt() })
 			sum += tmp
 		}
-
 		return sum
 	}
 
 	val input = readInput("Day12")
 	part1(input).println()
-//    part2(input).println()
+	part2(input).println()
 }
