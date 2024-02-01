f701e54920ec33d5c7c6cbee9dca8dc91cc08e04
flacchom <david.b.matejka@gmail.com>
2024-02-01 13:57:29 +0100

    adding boxes to 3dgrid and let them fall until they settle

--- src/Day22.kt
+++ src/Day22.kt
@@ -1,25 +1,50 @@
+
 import kotlin.math.abs
 import kotlin.math.max
 
 fun main() {
+	data class Point3(var x: Int, var y: Int, var z: Int)
 	data class Box(val p: List<Int>, val q: List<Int>) {
-		val dx = abs(p[0] - q[0])
-		val dy = abs(p[1] - q[1])
-		val dz = abs(p[2] - q[2])
-		val scalar = max(max(dx, dy), dz)
-
-		val vec = if (scalar != 0) {
-			Triple(dx/scalar, dy/scalar, dz/scalar)
-		} else {
-			Triple(0, 0, 0)
-		}
-
+		var higherPoint = emptyList<Int>()
 		val lowerPoint = if (p[2] <= q[2]) {
+			higherPoint = q
 			p
 		} else {
+			higherPoint = p
 			q
 		}
 
+		val dx = higherPoint[0] - lowerPoint[0]
+		val dy = higherPoint[1] - lowerPoint[1]
+		val dz = higherPoint[2] - lowerPoint[2]
+		val scalar = max(max(abs(dx), abs(dy)), abs(dz))
+
+		val vec = if (scalar != 0) {
+			Point3(dx/scalar, dy/scalar, dz/scalar)
+		} else {
+			Point3(0, 0, 0)
+		}
+
+//		val dx = abs(p[0] - q[0])
+//		val dy = abs(p[1] - q[1])
+//		val dz = abs(p[2] - q[2])
+//		val scalar = max(max(dx, dy), dz)
+//		val vec = if (scalar != 0) {
+//			Triple(dx/scalar, dy/scalar, dz/scalar)
+//		} else {
+//			Triple(0, 0, 0)
+//		}
+
+		fun getAllPoints():List<Point3> {
+			val allPoints = mutableListOf<Point3>()
+			for (i in 0..scalar) {
+				allPoints.add(Point3(
+					lowerPoint[0] + vec.x * i,
+					lowerPoint[1] + vec.y * i,
+					lowerPoint[2] + vec.z * i))
+			}
+			return allPoints
+		}
 		override fun toString(): String {
 			return "$p $q $vec $scalar"
 		}
@@ -40,9 +65,23 @@ fun main() {
 			}
 		}
 
-		fun settle(boxes: MutableList<Box>) {
-			boxes.forEach {
-				it.println()
+		fun addAndSettle(box: Box) {
+			val pointsToAdd = box.getAllPoints().toMutableList()
+			val cz = box.lowerPoint[2]
+			for (i in 0..cz) {
+				var canFall = true
+				pointsToAdd.forEach {
+					if (it.z == 1 || grid[it.x][it.y][it.z - 1] == 1) {
+						canFall = false
+					}
+				}
+				if (canFall) {
+					pointsToAdd.map { it.z -= 1 }
+				} else {
+					pointsToAdd.forEach {
+						grid[it.x][it.y][it.z] = 1
+					}
+				}
 			}
 
 		}
@@ -52,7 +91,7 @@ fun main() {
 				erg += "z_$k: \n"
 				for (i in 0..<dimX) {
 					for (j in 0..<dimY) {
-						erg += grid[i][j][k]
+						erg += "${grid[i][j][k]} "
 					}
 					erg += "\n"
 				}
@@ -90,11 +129,14 @@ fun main() {
 		val maxZ = maxDim[2] ?: 0
 		val grid = Grid3d(maxX + 1, maxY + 1, maxZ + 1)
 
-		boxes.forEach {
-			grid.add(it)
-		}
+//		boxes.forEach {
+//			grid.add(it)
+//		}
+//		println(grid)
+
+		boxes.forEach { grid.addAndSettle(it) }
+//		grid.addAndsettle(boxes[0])
 		println(grid)
-		grid.settle(boxes)
 		return -1
 	}
 
@@ -103,7 +145,7 @@ fun main() {
 		return -1
 	}
 
-	val input = readInput("Day22")
+	val input = readInput("Day22_test")
 	part1(input).println()
 //    part2(input).println()
 }
